a64ef0f6a11f36035568db2d0512f87dc9ab881b
Moe <129519233+Abdallah-Moe-dev@users.noreply.github.com>
2024-04-26 02:15:07 +0300

    Split function into separate files, added text preprocessing(remove stop words, convert to lowercase)

---
+++ pre_process/pre.process.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from convert_to_lowercase import convert_to_lowercase
+from remove_stop_words import remove_stop_words
+
+data = pd.read_csv('sentimentdataset.csv')
+
+data = convert_to_lowercase(data, 'Text')
+print(data['Text'].head())
+
+data['Text'] = data['Text'].apply(remove_stop_words)
+print(data['Text'].head())
---
+++ pre_process/remove_stop_words.py
@@ -0,0 +1,24 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import TweetTokenizer
+
+
+# func: .apply takes a function and applies it to all values of pandas series. 
+# convert_dtype: Convert dtype as per the function’s operation.
+# args=(): Additional arguments to pass to function instead of series.
+# Return Type: Pandas Series after applied function/operation.
+
+def remove_stop_words(text):
+    # make the whole sentence into small peices
+    tokenizer = TweetTokenizer()
+    # stop words for lang -> english
+    stop_words = stopwords.words('english')
+    sentence = tokenizer.tokenize(text)
+    sentence_without_stopwords = []
+    for word in sentence:
+        if word not in stop_words:
+            sentence_without_stopwords.append(word)
+    processed_text = ' '.join(sentence_without_stopwords)
+    # The line processed_text = ' '.join(tokens_without_stopwords) joins the
+    # list tokens_without_stopwords into a single string, separated by a space ' '.
+    return processed_text
