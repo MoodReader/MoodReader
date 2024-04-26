4eba5c8ec06e25e2747b0047d5ed4081a029f223
Moe <129519233+Abdallah-Moe-dev@users.noreply.github.com>
2024-04-26 12:11:47 +0300

    changed the .csv file location

--- pre_process/pre.process.py
+++ pre_process/pre.process.py
@@ -2,7 +2,7 @@ import pandas as pd
 from convert_to_lowercase import convert_to_lowercase
 from remove_stop_words import remove_stop_words
 
-data = pd.read_csv('sentimentdataset.csv')
+data = pd.read_csv('../sentimentdataset.csv')
 
 data = convert_to_lowercase(data, 'Text')
 print(data['Text'].head())
