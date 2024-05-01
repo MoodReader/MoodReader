ab010e62b8d66f82fca26dca015f04402b0c68ac
aliamgad <mostafaamgadaboseif@gmail.com>
2024-05-02 01:11:06 +0300

    merged

--- pre_process/pre_process.py
+++ pre_process/pre_process.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,39 +10,13 @@ import string
 from text_normalization import*
 from remove_slangs import*
 
-data = pd.read_csv('sentimentdataset.csv')
-
-for row in data.index:
-   data.loc[row,"Text"] = data.loc[row,"Text"].lower()
-
-for row in data.index:
-   data.loc[row,"Sentiment (Label)"] = data.loc[row,"Sentiment (Label)"].lower()
-   
-# Removing punctuation and stop words 
-punctuation = list(string.punctuation)
-for i in punctuation:
-    data["Text"] = data["Text"].str.replace(i, " ") 
-
-stop_words= stopwords.words('english')+ ["❤️" , "🐾", "🐶","💪","🎉","🎨","🎂"]
-for i in stop_words:
-    data["Text"] = data["Text"].str.replace(' '+i+' ', " ") 
-
-# remove extra spaces
-for row in data.index:
-   data.loc[row,"Text"] = " ".join(data.loc[row,"Text"].split())
-
-data["Sentiment (Label)"] = data["Sentiment (Label)"].apply(remove_slangs)
-
-data["Sentiment (Label)"] = data["Sentiment (Label)"].apply(Converting_To_Primitive)
-
-print(data["Text"])
-=======
 import pandas as pd
 from convert_to_lowercase import convert_to_lowercase
 from remove_stop_words import remove_stop_words
 from remove_punctuation import remove_punctuation
 from remove_slangs import remove_slangs
 from remove_extra_spaces import remove_extra_spaces
+
 data = pd.read_csv('../sentimentdataset.csv')
 
 data = remove_extra_spaces(data)
@@ -63,9 +36,23 @@ print(data['Text'].head())
 data['Text'] = data['Text'].apply(remove_slangs)
 print(data['Text'].head())
 
+# for row in data.index:
+#    data.loc[row,"Text"] = data.loc[row,"Text"].lower()
 
-# text = "Just published a new blog post. Check it out!"
+# for row in data.index:
+#    data.loc[row,"Sentiment (Label)"] = data.loc[row,"Sentiment (Label)"].lower()
+   
+# # Removing punctuation and stop words 
+# punctuation = list(string.punctuation)
+# for i in punctuation:
+#     data["Text"] = data["Text"].str.replace(i, " ") 
 
-# text = remove_punctuation(text)
-# print(text)
->>>>>>> origin/moe
+# stop_words= stopwords.words('english')+ ["❤️" , "🐾", "🐶","💪","🎉","🎨","🎂"]
+# for i in stop_words:
+#     data["Text"] = data["Text"].str.replace(' '+i+' ', " ") 
+
+
+
+data["Sentiment (Label)"] = data["Sentiment (Label)"].apply(remove_slangs)
+
+data["Sentiment (Label)"] = data["Sentiment (Label)"].apply(Converting_To_Primitive)
