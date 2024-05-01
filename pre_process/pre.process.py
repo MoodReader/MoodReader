dcd8c4c534e5b50d006564c96182cee05b59ef89
Moe <129519233+Abdallah-Moe-dev@users.noreply.github.com>
2024-05-01 22:39:04 +0300

    added function to remove slangs

--- pre_process/pre.process.py
+++ pre_process/pre.process.py
@@ -2,6 +2,7 @@ import pandas as pd
 from convert_to_lowercase import convert_to_lowercase
 from remove_stop_words import remove_stop_words
 from remove_punctuation import remove_punctuation
+from remove_slangs import remove_slangs
 data = pd.read_csv('../sentimentdataset.csv')
 
 data = convert_to_lowercase(data, 'Text')
@@ -15,7 +16,8 @@ data['Text'] = data['Text'].apply(remove_stop_words)
 print(data['Text'].head())
 
 
-
+data['Text'] = data['Text'].apply(remove_slangs)
+print(data['Text'].head())
 
 
 # text = "Just published a new blog post. Check it out!"
