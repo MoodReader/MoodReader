2e9f379703560c97f2666f60d77d4c25efc65178
Moe <129519233+Abdallah-Moe-dev@users.noreply.github.com>
2024-05-02 03:02:50 +0300

    added Converting_To_Primitive and lemmatization_text

--- pre_process/pre_process.py
+++ pre_process/pre_process.py
@@ -4,9 +4,12 @@ from remove_stop_words import remove_stop_words
 from remove_punctuation import remove_punctuation
 from remove_slangs import remove_slangs
 from remove_extra_spaces import remove_extra_spaces
+from lemmatization_text import lemmatization_text
+from text_normalization import Converting_To_Primitive
 data = pd.read_csv('../sentimentdataset.csv')
 
 data = remove_extra_spaces(data)
+
 
 
 data = convert_to_lowercase(data, 'Text')
@@ -23,7 +26,12 @@ print(data['Text'].head())
 data['Text'] = data['Text'].apply(remove_slangs)
 print(data['Text'].head())
 
+data['Sentiment (Label)'] = data['Sentiment (Label)'].apply(Converting_To_Primitive)
+print(data['Sentiment (Label)'].head())
 
+
+data['Text'] = data['Text'].apply(lemmatization_text)
+print(data['Text'].head())
 # text = "Just published a new blog post. Check it out!"
 
 # text = remove_punctuation(text)
